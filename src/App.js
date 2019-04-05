e9c78357ebe9b72b811ceeebd1df04cd7399c60a
Mario Souto <soutomarios@gmail.com>
2019-04-05 17:39:47 -0300

    project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import Cabecalho from './components/Cabecalho'
+import NavMenu from './components/NavMenu'
+import Dashboard from './components/Dashboard'
+import Widget from './components/Widget'
+import TrendsArea from './components/TrendsArea'
+import Tweet from './components/Tweet'
+import classNames from 'classnames'
+
+// function classNames(...possibleCSSClasses) {
+//   return possibleCSSClasses.map((possibleCSSClass) => {
+//     let className;
+    
+//     if(typeof possibleCSSClass === 'string') {
+//       className = possibleCSSClass
+//     }
+//     if(typeof possibleCSSClass === 'object' && !Array.isArray(possibleCSSClass)) {
+//       className = Object.keys(possibleCSSClass).filter((possibleCSSClassName) => {
+//         return possibleCSSClass[possibleCSSClassName]
+//       })
+//     }
+
+//     return className;
+//   }).join(' ')
+// }
+
 
 class App extends Component {
+  state = {
+    novoTweet: '',
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value, 
+    })
+  }
+
+  isNovoTweetLowerOrEqual140 = () => this.state.novoTweet.length <= 140
+  isNovoTweetLowerOrEqual0 = () => this.state.novoTweet.length <= 0
+
   render() {
+    // console.log(this.isNovoTweetHigherThan0() && this.isNovoTweetLowerThan140())
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <Cabecalho>
+            <NavMenu usuario="@omariosouto" />
+        </Cabecalho>
+        <div className="container">
+            <Dashboard>
+                <Widget>
+                    <form className="novoTweet">
+                        <div className="novoTweet__editorArea">
+                            <span className={classNames(
+                              'novoTweet__status',
+                              'outra classe',
+                              {
+                                'novoTweet__status--invalido': !this.isNovoTweetLowerOrEqual140()
+                              }
+                            )}>
+                              {this.state.novoTweet.length}/140
+                            </span>
+                            {/* <span className={`
+                              novoTweet__status
+                              ${
+                                !this.isNovoTweetLowerThan140()
+                                ? 'novoTweet__status--invalido' 
+                                : '' 
+                              }`}>
+                              {this.state.novoTweet.length}/140
+                            </span> */}
+                            <textarea
+                              onChange={this.handleChange}
+                              value={this.state.novoTweet}
+                              name="novoTweet"
+                              className="novoTweet__editor"
+                              placeholder="O que está acontecendo?"></textarea>
+                        </div>
+                        <button
+                          type="submit"
+                          disabled={!this.isNovoTweetLowerOrEqual140() || this.isNovoTweetLowerOrEqual0()}
+                          className="novoTweet__envia">
+                          Tweetar
+                        </button>
+                    </form>
+                </Widget>
+                <Widget>
+                    <TrendsArea />
+                </Widget>
+            </Dashboard>
+            <Dashboard posicao="centro">
+                <Widget>
+                    <div className="tweetsArea">
+                        <Tweet />
+                    </div>
+                </Widget>
+            </Dashboard>
+        </div>
+      </Fragment>
     );
   }
 }
